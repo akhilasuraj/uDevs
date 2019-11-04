b20c4ae25d107945039d285b36a32eb6b4b8b4d1
ThushanLJ <thushanlakshitha97@gmail.com>
2019-11-04 22:08:50 +0530

    ll

--- database/db.js
+++ database/db.js
@@ -1,17 +1,18 @@
 const Sequelize = require("sequelize")
 const db = {}
-const sequelize = new Sequelize("uDevs","root","",{
-    host:"localhost",
-    dialect:"mysql",
-    operatorsAliases: false,
+const sequelize = new Sequelize("mysql://b7ac7508864695:baa4a581@us-cdbr-iron-east-05.cleardb.net/heroku_68d032e51218807?reconnect=true")
+// const sequelize = new Sequelize("uDevs","root","",{
+//     host:"localhost",
+//     dialect:"mysql",
+//     operatorsAliases: false,
 
-    pool: {
-        max: 5,
-        min: 0,
-        aquire: 30000,
-        idle: 10000
-    }
-})
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         aquire: 30000,
+//         idle: 10000
+//     }
+// })
 
 db.sequelize = sequelize
 db.Sequelize = Sequelize 
